cef1f9e45354d6e72e90fa505861fab8c166306a
Bhagyashri-Birajdar <brbirajdar91@gmail.com>
2023-03-16 13:57:44 +0530

    Completed variable assignment
    Completed variable assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,27 @@
+console.log("*********Step 1**********");
+
+var myFullName;
+console.log(myFullName);
+
+var myFullName="Bhagyashri Birajdar";
+console.log("my full name is:",myFullName);
+
+var myFullName="Bhagyashri Raghunath Birajdar";
+console.log("my full name is:",myFullName);
+
+ console.log("*********Step 2**********");
+
+var myLove="I Love only JS";
+console.log(myLove);
+
+console.log("*********Step 3**********");
+
+var firstName="Bhagyashri";
+var lastName="Birajdar";
+var age="30";
+var address="Mumbai";
+var pincode="421518";
+var gender="Female";
+var isMarried="Yes";
+
+console.log(firstName,lastName,age,address,pincode,gender,isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,14 @@
 console.log("Bhagyashri Birajdar");
+console.log("31");
+console.log("Pune");
+
+var fullName="Bhagyashri Birajdar";
+console.log(fullName);
+console.log(fullName);
+
+var city;//variable declaration
+console.log(city);
+city="Pune"; //variable initialazation
+console.log(city);
+city="Mumbai";
+console.log(city);
